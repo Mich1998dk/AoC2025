cc66c1b668f831e2f18f2384d97a3c05172c26a5
Michael Maurer <mmau@netcompany.com>
2025-12-04 14:03:59 +0100

    day 4 part 2

--- src/main/java/aoc/days/Day04.java
+++ src/main/java/aoc/days/Day04.java
@@ -1,5 +1,6 @@
 package aoc.days;
 
+import java.util.Arrays;
 import java.util.List;
 
 import aoc.util.AocUtil;
@@ -26,20 +27,14 @@ public class Day04 {
                         counter += checkAbove(j, i, line);
                     }
                     counter += checkSameLine(j, line);
-                    if (i < sonar.size() -1) {
+                    if (i < sonar.size() - 1) {
                         counter += checkBelow(j, i, line);
                     }
                     if (counter < 4) {
                         sum++;
-                        //System.out.print(counter);
-                    } else {
-                        //System.out.print("@");
                     }
-                } else {
-                    //System.out.print(".");
                 }
             }
-            //System.out.println("");
         }
         return ""+sum;
     }
@@ -97,6 +92,88 @@ public class Day04 {
 
     public static String solvePart2() throws Exception {
         init();
-        return null;
+        char[][] grid = sonar.stream().map(String::toCharArray).toArray(char[][]::new);
+        int sum = 0;
+        int removed = 1;
+        while (removed > 0) {
+            removed = 0;
+            for (int i = 0; i < grid.length; i++) {
+                char[] line = grid[i];
+                for (int j = 0; j < line.length; j++) {
+                    char slot = line[j];
+                    int counter = 0;
+                    if (slot == '@') {
+                        if (i > 0) {
+                            counter += checkAbove2(j, i, line, grid);
+                        }
+                        counter += checkSameLine2(j, line);
+                        if (i < sonar.size() - 1) {
+                            counter += checkBelow2(j, i, line, grid);
+                        }
+                        if (counter < 4) {
+                            sum++;
+                            grid[i][j] = 'X';
+                            removed++;
+                        }
+                    }
+                }
+            }
+
+            grid = Arrays.stream(grid)
+                    .map(row -> new String(row).replace('X', '.').toCharArray())
+                    .toArray(char[][]::new);
+        }
+        return sum+"";
+    }
+
+    private static int checkSameLine2(int j, char[] line) {
+        int counter = 0;
+        if (j != 0) {
+            if (line[j-1] == '@' || line[j-1] == 'X') {
+                counter++;
+            }
+        }
+        if (j != line.length - 1) {
+            if (line[j + 1] == '@' || line[j + 1] == 'X') {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    private static int checkAbove2(int j, int i, char[] line, char[][] grid) {
+        int counter = 0;
+        if (j > 0) {
+            if (grid[i-1][j-1] == '@' || grid[i-1][j-1] == 'X') {
+                counter++;
+            }
+        }
+        if (grid[i-1][j] == '@' || grid[i-1][j] == 'X') {
+            counter++;
+        }
+        if (j < line.length-1) {
+            if (grid[i-1][j+1] == '@' || grid[i-1][j+1] == 'X') {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    private static int checkBelow2(int j, int i, char[] line, char[][] grid) {
+        int counter = 0;
+        if (j > 0) {
+            if (grid[i+1][j-1] == '@' || grid[i+1][j-1] == 'X') {
+                counter++;
+            }
+        }
+        if (grid[i+1][j] == '@' || grid[i+1][j] == 'X') {
+            counter++;
+        }
+        if (j < line.length - 1) {
+            if (grid[i+1][j+1] == '@' || grid[i+1][j+1] == 'X') {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
